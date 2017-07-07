0af9f29b2b129642e0bf7e2cc5747896c8d7c396
mariannk <marianna.k@nopcommerce.com>
2017-07-07 18:18:17 +0300

    Working on tag helpers for admin area

--- src/Presentation/Nop.Web.Framework/TagHelpers/NopInputTagHelper.cs
+++ src/Presentation/Nop.Web.Framework/TagHelpers/NopInputTagHelper.cs
@@ -8,22 +8,46 @@ namespace Nop.Web.Framework.TagHelpers
     public class NopInputTagHelper : InputTagHelper
     {
         private const string ForAttributeName = "asp-for";
+        private const string DisabledAttributeName = "asp-disabled";
+        private const string RequiredAttributeName = "asp-required";
 
-        [HtmlAttributeName("asp-disabled")]
+        [HtmlAttributeName(DisabledAttributeName)]
         public string IsDisabled { set; get; }
+
+        [HtmlAttributeName(RequiredAttributeName)]
+        public string IsRequired { set; get; }
 
         public NopInputTagHelper(IHtmlGenerator generator) : base(generator)
         {
+
         }
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
+            //disabled attribute
             bool.TryParse(IsDisabled, out bool disabled);
             if (disabled)
             {
                 var d = new TagHelperAttribute("disabled", "disabled");
                 output.Attributes.Add(d);
             }
+
+            //merge classes
+            var classValue = output.Attributes.ContainsName("class")
+                ? $"{output.Attributes["class"].Value} form-control"
+                : "form-control";
+            output.Attributes.SetAttribute("class", classValue);
+
+            //required asterisk
+            bool.TryParse(IsRequired, out bool required);
+            if (required)
+            {
+                output.PreElement.SetHtmlContent("<div class='input-group input-group-required'>");
+                output.PostElement.SetHtmlContent("<div class=\"input-group-btn\"><span class=\"required\">*</span></div></div>");
+            }
+
+            //editor template
+            var modelTypeName = For.Metadata.ModelType.Name;
 
             base.Process(context, output);
         }
