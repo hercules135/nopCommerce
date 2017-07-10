ec0b503536ee7f6de9e20644d90eff045d47891d
mariannk <marianna.k@nopcommerce.com>
2017-07-10 17:36:28 +0300

    Further changes in tag-helpers. Further changes on the category create/update page

--- src/Presentation/Nop.Web.Framework/TagHelpers/NopInputTagHelper.cs
+++ src/Presentation/Nop.Web.Framework/TagHelpers/NopInputTagHelper.cs
@@ -1,15 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc.TagHelpers;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 
 namespace Nop.Web.Framework.TagHelpers
 {
-    [HtmlTargetElement("input", Attributes = ForAttributeName)]
+    [HtmlTargetElement("nop-editor", Attributes = ForAttributeName)]
     public class NopInputTagHelper : InputTagHelper
     {
         private const string ForAttributeName = "asp-for";
         private const string DisabledAttributeName = "asp-disabled";
         private const string RequiredAttributeName = "asp-required";
+        private const string TemplateAttributeName = "asp-template";
+        private const string ItemsAttributeName = "asp-items";
+
+        private readonly IHtmlHelper _htmlHelper;
 
         [HtmlAttributeName(DisabledAttributeName)]
         public string IsDisabled { set; get; }
@@ -17,13 +27,21 @@ namespace Nop.Web.Framework.TagHelpers
         [HtmlAttributeName(RequiredAttributeName)]
         public string IsRequired { set; get; }
 
-        public NopInputTagHelper(IHtmlGenerator generator) : base(generator)
-        {
+        [HtmlAttributeName(TemplateAttributeName)]
+        public string Template { set; get; }
 
+        [HtmlAttributeName(ItemsAttributeName)]
+        public IList<SelectListItem> Items { set; get; }
+
+        public NopInputTagHelper(IHtmlGenerator generator, IHtmlHelper htmlHelper) : base(generator)
+        {
+            _htmlHelper = htmlHelper;
         }
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
+            output.SuppressOutput();
+
             //disabled attribute
             bool.TryParse(IsDisabled, out bool disabled);
             if (disabled)
@@ -33,10 +51,10 @@ namespace Nop.Web.Framework.TagHelpers
             }
 
             //merge classes
-            var classValue = output.Attributes.ContainsName("class")
-                ? $"{output.Attributes["class"].Value} form-control"
-                : "form-control";
-            output.Attributes.SetAttribute("class", classValue);
+            //var classValue = output.Attributes.ContainsName("class")
+            //    ? $"{output.Attributes["class"].Value} form-control"
+            //    : "form-control";
+            //output.Attributes.SetAttribute("class", classValue);
 
             //required asterisk
             bool.TryParse(IsRequired, out bool required);
@@ -46,10 +64,19 @@ namespace Nop.Web.Framework.TagHelpers
                 output.PostElement.SetHtmlContent("<div class=\"input-group-btn\"><span class=\"required\">*</span></div></div>");
             }
 
-            //editor template
-            var modelTypeName = For.Metadata.ModelType.Name;
+            //generate editor
+            var viewContextAware
+                = _htmlHelper as IViewContextAware;
+            viewContextAware?.Contextualize(ViewContext);
 
-            base.Process(context, output);
+            IHtmlContent s = _htmlHelper.Editor(For.Name, Template, Items != null && Items.Any() ? new { SelectList = Items } : null);
+            string htmlOutput;
+            using (var writer = new StringWriter())
+            {
+                s.WriteTo(writer, HtmlEncoder.Default);
+                htmlOutput = writer.ToString();
+            }
+            output.Content.SetHtmlContent(htmlOutput);
         }
     }
 }
--- src/Presentation/Nop.Web.Framework/TagHelpers/NopSelectTagHelper.cs
+++ src/Presentation/Nop.Web.Framework/TagHelpers/NopSelectTagHelper.cs
@@ -4,19 +4,26 @@ using Microsoft.AspNetCore.Razor.TagHelpers;
 
 namespace Nop.Web.Framework.TagHelpers
 {
-    [HtmlTargetElement("nop-select")]
+    [HtmlTargetElement("nop-select", Attributes = ForAttributeName)]
     public class NopSelectTagHelper : SelectTagHelper
     {
+        private const string ForAttributeName = "asp-for";
+
         public NopSelectTagHelper(IHtmlGenerator generator) : base(generator)
         {
         }
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
+            var formControlClass = "form-control";
+
+            //merge classes
             var classValue = output.Attributes.ContainsName("class")
-                                ? $"{output.Attributes["class"].Value} form-control"
-                                : "form-control";
+                ? $"{output.Attributes["class"].Value} {formControlClass}"
+                : formControlClass;
             output.Attributes.SetAttribute("class", classValue);
+
+            //tag details
             output.TagName = "select";
             output.TagMode = TagMode.StartTagAndEndTag;
 
